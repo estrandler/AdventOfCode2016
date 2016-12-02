344ed2c72da58544ebfe7b8a1d27b3cb8546a647
Erik Strandler <erik@strandler.se>
2016-12-02 09:44:33 +0100

    lvl 2 part 2

--- src/LevelTwo/LevelTwo.js
+++ src/LevelTwo/LevelTwo.js
@@ -2,7 +2,8 @@
 var fs = require('fs');
 console.log('DayTwo!');
 var Level2 = (function () {
-    function Level2() {
+    function Level2(shouldSolvePlus) {
+        //Part one
         this.row1 = [1, 2, 3];
         this.row2 = [4, 5, 6];
         this.row3 = [7, 8, 9];
@@ -11,13 +12,74 @@ var Level2 = (function () {
         this.minIndex = 0;
         this.maxIndex = 2;
         this.grid = [this.row1, this.row2, this.row3];
+        //Part two
+        this.row1_plus = [0, 0, 1, 0, 0];
+        this.row2_plus = [0, 2, 3, 4, 0];
+        this.row3_plus = [5, 6, 7, 8, 9];
+        this.row4_plus = [0, 'A', 'B', 'C', 0];
+        this.row5_plus = [0, 0, 'D', 0, 0];
+        this.x_plus = 0;
+        this.y_plus = 2;
+        this.minIndex_plus = 0;
+        this.maxIndex_plus = 4;
+        this.grid_plus = [this.row1_plus, this.row2_plus, this.row3_plus, this.row4_plus, this.row5_plus];
+        //Common
         this.Left = 'L';
         this.Right = 'R';
         this.Up = 'U';
         this.Down = 'D';
         this.code = '';
-        this.solve();
+        if (!shouldSolvePlus)
+            this.solve();
+        else
+            this.solvePlus();
     }
+    Level2.prototype.solvePlus = function () {
+        var self = this;
+        fs.readFile('src/LevelTwo/LevelTwoInput.txt', "utf8", function (err, data) {
+            var rows = data.split('\n');
+            for (var j = 0; j < rows.length; j++) {
+                for (var i = 0; i < rows[j].length; i++) {
+                    var instruction = rows[j][i];
+                    if (instruction == self.Left)
+                        self.moveleft_plus();
+                    else if (instruction == self.Right)
+                        self.moveright_plus();
+                    else if (instruction == self.Up)
+                        self.moveup_plus();
+                    else if (instruction == self.Down)
+                        self.movedown_plus();
+                }
+                self.code += self.grid_plus[self.y_plus][self.x_plus];
+            }
+            console.log('Level2_plus:');
+            console.log('Result:', self.code);
+        });
+    };
+    Level2.prototype.moveup_plus = function () {
+        var yAfterMove = this.y_plus - 1;
+        if (yAfterMove >= this.minIndex_plus && this.grid_plus[yAfterMove][this.x_plus] !== 0) {
+            this.y_plus = yAfterMove;
+        }
+    };
+    Level2.prototype.movedown_plus = function () {
+        var yAfterMove = this.y_plus + 1;
+        if (yAfterMove <= this.maxIndex_plus && this.grid_plus[yAfterMove][this.x_plus] !== 0) {
+            this.y_plus = yAfterMove;
+        }
+    };
+    Level2.prototype.moveleft_plus = function () {
+        var xAfterMove = this.x_plus - 1;
+        if (xAfterMove >= this.minIndex_plus && this.grid_plus[this.y_plus][xAfterMove] !== 0) {
+            this.x_plus = xAfterMove;
+        }
+    };
+    Level2.prototype.moveright_plus = function () {
+        var xAfterMove = this.x_plus + 1; //this.x_plus + 1;
+        if (xAfterMove <= this.maxIndex_plus && this.grid_plus[this.y_plus][xAfterMove] !== 0) {
+            this.x_plus = xAfterMove;
+        }
+    };
     Level2.prototype.solve = function () {
         var self = this;
         fs.readFile('src/LevelTwo/LevelTwoInput.txt', "utf8", function (err, data) {
@@ -36,6 +98,7 @@ var Level2 = (function () {
                 }
                 self.code += self.grid[self.y][self.x];
             }
+            console.log('Level2:');
             console.log('Result:', self.code);
         });
     };
@@ -57,5 +120,6 @@ var Level2 = (function () {
     };
     return Level2;
 }());
-var lvl = new Level2();
+var lvl = new Level2(false);
+var lvl_plus = new Level2(true);
 //# sourceMappingURL=LevelTwo.js.map
--- src/LevelTwo/LevelTwo.ts
+++ src/LevelTwo/LevelTwo.ts
@@ -6,6 +6,7 @@ console.log('DayTwo!');
 
 
 class Level2 {
+    //Part one
     private row1: number[] = [1, 2, 3];
     private row2: number[] = [4, 5, 6];
     private row3: number[] = [7, 8, 9];
@@ -15,16 +16,98 @@ class Level2 {
     private maxIndex: number = 2;
     private grid: number[][] = [this.row1, this.row2, this.row3];
 
+    //Part two
+    private row1_plus: number[] = [0, 0, 1, 0, 0];
+    private row2_plus: number[] = [0, 2, 3, 4, 0];
+    private row3_plus: number[] = [5, 6, 7, 8, 9];
+    private row4_plus: any[] = [0, 'A', 'B', 'C', 0];
+    private row5_plus: any[] = [0, 0, 'D', 0, 0];
+    private x_plus: number = 0;
+    private y_plus: number = 2;
+    
+    private minIndex_plus: number = 0;
+    private maxIndex_plus: number = 4;
+    private grid_plus: any[][] = [this.row1_plus, this.row2_plus, this.row3_plus, this.row4_plus, this.row5_plus];
+
+    //Common
     private Left: string = 'L';
     private Right: string = 'R';
     private Up: string = 'U';
     private Down: string = 'D';
-
     private code: string = '';
 
-    constructor() {
-         this.solve();
+    constructor(shouldSolvePlus: boolean) {
+        if(!shouldSolvePlus)
+            this.solve();
+        else
+            this.solvePlus();
     }
+
+    private solvePlus(){
+        var self = this;
+        fs.readFile('src/LevelTwo/LevelTwoInput.txt', "utf8", function(err, data) {
+            var rows = data.split('\n');
+
+            for(var j = 0; j < rows.length; j++){
+                for(var i = 0; i < rows[j].length; i++){
+                    var instruction = rows[j][i];
+                   
+
+                    if(instruction == self.Left)
+                        self.moveleft_plus();
+                    else if(instruction == self.Right)
+                        self.moveright_plus();
+                    else if(instruction == self.Up)
+                        self.moveup_plus();
+                    else if(instruction == self.Down)
+                        self.movedown_plus();
+
+                }
+
+                self.code += self.grid_plus[self.y_plus][self.x_plus];
+            }
+
+            
+            console.log('Level2_plus:');
+            console.log('Result:', self.code);
+        });
+    }
+
+    private moveup_plus(){
+       let yAfterMove = this.y_plus - 1;
+            if(yAfterMove >= this.minIndex_plus && this.grid_plus[yAfterMove][this.x_plus] !== 0){
+                this.y_plus = yAfterMove;
+            }
+       
+    }
+
+    private movedown_plus(){
+
+
+        let yAfterMove = this.y_plus + 1;
+        
+
+         if(yAfterMove <= this.maxIndex_plus && this.grid_plus[yAfterMove][this.x_plus] !== 0){
+             this.y_plus = yAfterMove;
+         }
+    }
+
+    private moveleft_plus(){
+         let xAfterMove = this.x_plus - 1;
+
+          if(xAfterMove >= this.minIndex_plus && this.grid_plus[this.y_plus][xAfterMove] !== 0){
+             this.x_plus = xAfterMove;
+         }
+    }
+
+    private moveright_plus(){
+         let xAfterMove = this.x_plus + 1; //this.x_plus + 1;
+
+         if(xAfterMove <= this.maxIndex_plus && this.grid_plus[this.y_plus][xAfterMove] !== 0){
+             this.x_plus = xAfterMove;
+         }
+    }
+
 
     private solve(){
         var self = this;
@@ -47,9 +130,8 @@ class Level2 {
 
                 self.code += self.grid[self.y][self.x];
             }
-
             
-
+            console.log('Level2:');
             console.log('Result:', self.code);
         });
     }
@@ -75,5 +157,7 @@ class Level2 {
     }
 }
 
+var lvl = new Level2(false); 
 
-var lvl = new Level2();
+
+var lvl_plus = new Level2(true); 
